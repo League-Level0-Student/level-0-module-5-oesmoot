0f07bd655753b834dad44aa9210ac9f7dfde2341
league <league@wts14-3.attlocal.net>
2023-09-16 11:28:22 -0700

    e

--- src/_01_algorithms/_2_fibonacci/fibonacci.java
+++ src/_01_algorithms/_2_fibonacci/fibonacci.java
@@ -1,5 +1,17 @@
 package _01_algorithms._2_fibonacci;
 
 public class fibonacci {
-
+public static void main(String[] args) {
+	int num1 = 0;
+	int num2 = 1;
+	int sum = 0;
+	System.out.println(num1);
+	System.out.println(num2);
+	for(int i = 0; i < 10; i++) {
+		sum = num1 + num2;
+		System.out.println(sum + " ");
+		num1 = num2;
+		num2 = sum;
+	}
+}
 }
--- src/_01_algorithms/_3_goofy_names/GoofyNames.java
+++ src/_01_algorithms/_3_goofy_names/GoofyNames.java
@@ -11,28 +11,36 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+		String regularName = JOptionPane.showInputDialog("enter a name please.");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
-
+		System.out.println(regularName.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 7).
 		//    HINT: Use .length() to determine the number of characters in the String.
+		for (int i = 0; i < regularName.length(); i++) {
 
-				// 4. Create a char variable to store the next character of the name
-				//    use .charAt()
-	
-				
-				// 5. Use MODULO operator (%) to identify if it is an EVEN or ODD character.
-			
-			
+
+			// 4. Create a  variable to store the next character of the name
+			//    use .charAt()
+			char c = regularName.charAt(i);
+			System.out.println(c);
+			// 5. Use MODULO operator (%) to identify if it is an EVEN or ODD character.
+			if (i % 2 == 0) {
+			goofyName += Character.toLowerCase(c);
+			}
+			else {
+			goofyName += Character.toUpperCase(c);
+			}
+
 				// 6. Even characters should be made uppercase and odd characters made lowercase
 				//    HINT: use Character.toUpperCase() or Character.toLowerCase()
-	
-			
+
+
 				// 7. ADD the char to the end of the goofyName String
 
-		
+		}
 		// 8. Use pop-up to show user their Goofy name
+		JOptionPane.showMessageDialog(null, goofyName);
 
 	}
 }
